883dbbf2187e45ad2c464559cd87f4ba030b7e22
hoodarohit <hooda.r@northeastern.edu>
2023-03-23 11:58:49 -0400

    Added the labs

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import HelloWorld from "./labs/a6/HelloWorld";
 import Tuiter from "./tuiter";
 import {BrowserRouter} from "react-router-dom";
 import {Route, Routes} from "react-router";
+import Assignment7 from "./labs/a7";
 
 function App() {
     return (
@@ -11,6 +12,7 @@ function App() {
                 <Routes>
                     <Route index element={<Labs/>}/>
                     <Route path="/hello" element={<HelloWorld/>}/>
+                    <Route path="/a7" element={<Assignment7/>}/>
                     <Route path="/tuiter/*" element={<Tuiter/>}/>
                 </Routes>
             </div>
